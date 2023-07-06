e4047f1b2d90351c375fb28ffe5bb5e7a894299f
aguscontino <agusconti1994@gmail.com>
2023-07-05 21:05:59 -0300

    componente navbar

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,24 @@
+import "./style.css"
+
+function Navbar() {
+
+   return (
+
+      <nav>
+
+         <a href="./index.html">
+            <h1>Lunática</h1>
+         </a>
+
+         <ul>
+            <li><a href="">Home</a></li>
+            <li><a href="">Tienda</a></li>
+            <li><a href="">Contacto</a></li>
+         </ul>
+
+      </nav>
+
+   )
+}
+
+export default Navbar
