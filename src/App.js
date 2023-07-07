ed4a03e55009b54f61cd7f3bfc78a8df53c8ec45
aguscontino <agusconti1994@gmail.com>
2023-07-06 22:04:05 -0300

    item list container

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import Navbar from './components/Navbar/Navbar.jsx';
+import ItemListContainer from './components/ItemListContainer/ItemListContainer';
 
 function App() {
    return (
@@ -8,6 +9,7 @@ function App() {
          <header>
             <Navbar />
          </header>
+         <ItemListContainer saludo="¡Bienvenidos a Lunatica!" />
          
       </div>
    );
---
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -0,0 +1,13 @@
+import "./style.scss"
+
+function ItemListContainer(props) {
+
+   return (
+      <div className="ItemListContainer">
+         <p>{props.saludo}</p>
+      </div>
+   )
+
+}
+
+export default ItemListContainer
